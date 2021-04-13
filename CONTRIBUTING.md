b282b998cdd9c494f76ccf8cf43012edd67d5cf0
Axel S <contact@axelsimonet.fr>
2021-04-13 15:53:19 +0200

    Core: update docs & add starter template (#4)
    * feat(docs): contributing enhancement
    
    * feat(starter): add gulp
    
    * feat(starter): fix & add browserlist
    
    * feat(docs): contributing starter
    
    Co-authored-by: Stallos11 <vincentdelev@yahoo.fr>

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -25,7 +25,38 @@ In order to not have conflict with other plugins, we need specific rules on clas
     - The plugin class have to extend AxentixComponent
     - Use the Axentix.Config.registerPlugin method to register your plugin in Axentix
 
+  - Extend AxentixComponent details :
+
+Extending AxentixComponent allows to get Axentix features working on the plugin as the `sync()` , `reset()`, data init...
+```js
+// Plugin class example
+class PluginName extends AxentixComponent {
+    static getDefaultOptions() {
+      return {};
+    }
+    // Constructor example
+    constructor(element, options) {
+      super();
+
+      try {
+        this.preventDbInstance(element);
+        Axentix.instances.push({ type: 'PluginName', instance: this });
+        
+        this._setup();
+      } catch (error) {
+        console.error('[Axentix] {PluginName} init error', error);
+      }
+    }
+    // Needed methods
+    _setup()
+    _setupListeners()
+    _removeListeners()
+  }
+}
+```
   - Register function parameters :
+
+This method allows you to register your component as an Axentix component, which makes you able to create an instance with  `new Axentix.PluginName`...
 ```js
 Axentix.Config.registerPlugin({
   class: {ClassName},
@@ -45,6 +76,20 @@ https://useaxentix.com/docs/plugins
 
 ## Starter
 
-Using a starter template isn't mandatory, but recommended.
+The starter template allows you to **compile your CSS & JS** in one file, and your SASS/SCSS to CSS.
 
-We provide a package.json to automatically compile your JS and SCSS.
+Using a starter template isn't mandatory, but **recommended** since you'll have to create a package.json with **all things** that are inside the example one.
+
+Using the starter template, you have to use **this schema** :
+
+```
+├─┬ examples
+│ ├── index.html
+│
+├─┬ src
+  ├─┬ scss
+  │ ├─ {PluginName}.scss
+  │
+  ├─┬ js
+    ├─ script.js
+```
---
+++ starter/gulpfile.js
@@ -0,0 +1,104 @@
+const { src, dest, watch, series, parallel } = require('gulp'),
+  babel = require('gulp-babel'),
+  concat = require('gulp-concat'),
+  sass = require('gulp-sass'),
+  rename = require('gulp-rename'),
+  minify = require('gulp-babel-minify'),
+  browserSync = require('browser-sync').create(),
+  autoprefixer = require('gulp-autoprefixer'),
+  insert = require('gulp-insert'),
+  deporder = require('gulp-deporder'),
+  packageJson = require('./package.json'),
+  Fiber = require('fibers');
+
+sass.compiler = require('sass');
+
+const pluginName = packageJson['ax-plugins'].name.toLowerCase();
+
+function compileJSMinified() {
+  return src('src/js/**/*.js')
+    .pipe(deporder())
+    .pipe(concat(`${pluginName}.min.js`))
+    .pipe(
+      babel({
+        presets: ['@babel/env'],
+      })
+    )
+    .pipe(
+      minify({
+        mangle: {
+          keepClassName: true,
+        },
+      })
+    )
+    .pipe(dest('dist/js/'))
+    .pipe(browserSync.stream());
+}
+
+function compileJS() {
+  return src('src/js/**/*.js')
+    .pipe(deporder())
+    .pipe(concat(`${pluginName}.js`))
+    .pipe(
+      babel({
+        presets: ['@babel/env'],
+      })
+    )
+    .pipe(dest('dist/js/'))
+    .pipe(browserSync.stream());
+}
+
+function compileSassMinified() {
+  return src('src/scss/*.scss')
+    .pipe(sass({ fiber: Fiber, outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(dest('dist/css/'))
+    .pipe(browserSync.stream());
+}
+
+function compileSass() {
+  return src('src/scss/*.scss')
+    .pipe(sass({ fiber: Fiber }).on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(dest('dist/css/'))
+    .pipe(browserSync.stream());
+}
+
+function initBrowserSync() {
+  browserSync.init({
+    server: './',
+  });
+  watch(['examples/**/*.html', 'examples/**/*.css', 'examples/**/*.js']).on('change', browserSync.reload);
+}
+
+exports.jsmin = compileJSMinified;
+exports.js = compileJS;
+exports.sassmin = compileSassMinified;
+exports.sass = compileSass;
+
+exports.watch = function () {
+  initBrowserSync();
+  watch('src/js/**/*.js', series(compileJSMinified, compileJS));
+  watch('src/scss/**/*.scss', series(compileSassMinified, compileSass));
+};
+
+exports.watchjs = function () {
+  initBrowserSync();
+  watch('src/js/**/*.js', series(compileJSMinified, compileJS));
+};
+
+exports.watchsass = function () {
+  initBrowserSync();
+  watch('src/scss/**/*.scss', series(compileSassMinified, compileSass));
+};
+
+exports.default = parallel(compileJSMinified, compileJS, compileSassMinified, compileSass);
